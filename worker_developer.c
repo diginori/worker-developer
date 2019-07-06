43e15d9ee984baefa770ad40630d41a8a7835350
Kook Mul <good@diginori.com>
2019-07-06 10:25:39 +0900

    Create worker_developer.c

---
+++ worker_developer.c
@@ -0,0 +1,18 @@
+#include <헌법.h>
+#include <민주주의.h>
+
+#include "노동조합.h"
+
+#include "diginori.github.io"
+
+HumanLove main(char *participation)
+{
+  인류 *노동자;
+  HumanLovekjk h;
+  
+  GetHuman(노동자->개발자.검색왕.국물, &h)
+  
+  printf(h);
+
+  return HumanLove;
+}
